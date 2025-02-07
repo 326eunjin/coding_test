db0aaa092e19446be406b9b30a12a4e6d5486ebc
Eunjin Jang <326eunjin@naver.com>
2025-02-07 23:24:57 +0900

    [Silver I] Title: 단어 맞추기, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/9081. 단어 맞추기/단어 맞추기.py
+++ 백준/Silver/9081. 단어 맞추기/단어 맞추기.py
@@ -1,45 +1,24 @@
-def next_permutation(s):
-    # 문자열을 리스트로 변환
-    s = list(s)
-    n = len(s)
-
-    # 1. 뒤에서부터 첫 번째 감소하는 부분을 찾는다.
-    i = n - 2
-    while i >= 0 and s[i] >= s[i + 1]:
-        i -= 1
-
-    if i == -1:
-        return None  # 더 이상 순열이 없으면 None을 리턴
-
-    # 2. 그 부분보다 큰 숫자 중 가장 작은 숫자를 찾아서 교환
-    j = n - 1
-    while s[j] <= s[i]:
-        j -= 1
-
-    # 3. 교환
-    s[i], s[j] = s[j], s[i]
-
-    # 4. 뒤의 부분을 오름차순으로 정렬
-    s[i + 1:] = sorted(s[i + 1:])
-    
-    return ''.join(s)
-
-i = int(input())
-l = [input() for _ in range(i)]
+n = int(input())
+l = list(input() for _ in range(n))
 
 result = []
 
+# 뒤에서부터 첫 번째로 감소하는 부분을 찾음
 for s in l:
     flag = False
+    s = list(s)
     for i in range(-2, -len(s) - 1, -1):
-        tmp = s[i:]
-        next_per = next_permutation(tmp)
-        if next_per and next_per != tmp:
-            result.append(s[: len(s) - len(tmp)] + next_per)
-            flag = True
+        if s[i] < s[i + 1]:
+            # 그 다음 큰 숫자를 찾아 교환
+            for j in range(-1, -len(s) - 1, -1):
+                if s[j] > s[i]:
+                    s[j], s[i] = s[i], s[j]
+                    result.append(s[: i + 1] + sorted(s[i + 1 :]))
+                    flag = True
+                    break
             break
-    if not flag:
+    if flag == False:
         result.append(s)
 
 for i in result:
-    print(i)
+    print(''.join(i))
