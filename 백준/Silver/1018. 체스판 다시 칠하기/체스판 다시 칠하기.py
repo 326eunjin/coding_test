7b18542366cfef0957e0d54ae0086e2b1ebfa792
Eunjin Jang <326eunjin@naver.com>
2025-08-08 21:36:59 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 56 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
@@ -28,35 +28,13 @@ for i in range(n):
         else:
             origin_l[i][j] = 1
 result = n * m
-if n > 8 and m > 8:
-    for r_start in range(n - 7):
-        for c_start in range(m - 7):
-            sub_matrix = []
-            for row in origin_l[r_start : r_start + 8]:
-                sub_matrix.append(row[c_start : c_start + 8])
-            tmp_result = get_min_change(sub_matrix)
-            if tmp_result <= result:
-                result = tmp_result
-elif n == 8 and m > 8:
-    for r_start in range(1):
-        for c_start in range(m - 7):
-            sub_matrix = []
-            for row in origin_l[r_start : r_start + 8]:
-                sub_matrix.append(row[c_start : c_start + 8])
-            tmp_result = get_min_change(sub_matrix)
-            if tmp_result <= result:
-                result = tmp_result
-elif m == 8 and n > 8:
-    for r_start in range(n - 7):
-        for c_start in range(1):
-            sub_matrix = []
-            for row in origin_l[r_start : r_start + 8]:
-                sub_matrix.append(row[c_start : c_start + 8])
-            tmp_result = get_min_change(sub_matrix)
-            if tmp_result <= result:
-                result = tmp_result
-else:
-    tmp_result = get_min_change(origin_l)
-    if tmp_result <= result:
-        result = tmp_result
+
+for r_start in range(n - 7):
+    for c_start in range(m - 7):
+        sub_matrix = []
+        for row in origin_l[r_start : r_start + 8]:
+            sub_matrix.append(row[c_start : c_start + 8])
+        tmp_result = get_min_change(sub_matrix)
+        if tmp_result <= result:
+            result = tmp_result
 print(result)
